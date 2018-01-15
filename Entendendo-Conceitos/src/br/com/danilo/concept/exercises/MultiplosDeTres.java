b96b1e0dacc8d57c689413b2b7f019a21e4479ce
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-15 08:31:18 -0200

    Exercicio 5

---
+++ Entendendo-Conceitos/src/br/com/danilo/concept/exercises/MultiplosDeTres.java
@@ -0,0 +1,11 @@
+package br.com.danilo.concept.exercises;
+
+public class MultiplosDeTres {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			if(i % 3 == 0) {
+				System.out.println("Esse numero é multiplo de 3: " + i);
+			}
+		}
+	}
+}
