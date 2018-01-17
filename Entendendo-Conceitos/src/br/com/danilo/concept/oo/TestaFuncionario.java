ec2895d523e527a3524f1687ee863ae85bd23029
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-17 10:50:19 -0200

    Orientação a Objetos - Exercicio 4

--- Entendendo-Conceitos/src/br/com/danilo/concept/oo/TestaFuncionario.java
+++ Entendendo-Conceitos/src/br/com/danilo/concept/oo/TestaFuncionario.java
@@ -14,5 +14,24 @@ public class TestaFuncionario {
 		System.out.println("Ganho anual: " + righetto.calculaGanhoAnual());
 		
 		righetto.mostra();
+		
+		Funcionario lucas = new Funcionario();
+		
+		lucas.setSalario(1300.30);
+		lucas.setDepartamento("TI");
+		lucas.setNomeCompleto("Lucas Santos");
+		lucas.setRg("1234567-8");
+		lucas.recebeAumento(200);
+		
+		System.out.println("Salario com aumento: " + lucas.getSalario());
+		System.out.println("Ganho anual: " + lucas.calculaGanhoAnual());
+		
+		lucas.mostra();
+		
+		if(righetto == lucas) {
+			System.out.println("São Iguais");
+		}else {
+			System.out.println("São Diferentes");
+		}
 	}
 }
