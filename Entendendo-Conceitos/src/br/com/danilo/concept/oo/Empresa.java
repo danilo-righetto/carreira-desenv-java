8a154cdab94e4873875c4c4e3ef078977e29fb47
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-17 18:49:19 -0200

    Arrays - Exercicio 2 e 3

--- Entendendo-Conceitos/src/br/com/danilo/concept/oo/Empresa.java
+++ Entendendo-Conceitos/src/br/com/danilo/concept/oo/Empresa.java
@@ -4,4 +4,14 @@ public class Empresa {
 	String nome;
 	String cnpj;
 	Funcionario[] funcionarios;
+	int livre = 0;
+	
+	public void adicionar(Funcionario f) {
+		this.funcionarios[this.funcionarios.length+1] = f;
+		/* Outro modo: 
+		 * 
+		 * this.funcionarios[livre] = f;
+		 * livre++;
+		 * */
+	}
 }
