891adf31a688b3759f686f77e51eee1d9fb45511
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-17 10:19:32 -0200

    Orientação a Objetos - Exercicio 2

---
+++ Entendendo-Conceitos/src/br/com/danilo/concept/oo/TestaFuncionario.java
@@ -0,0 +1,16 @@
+package br.com.danilo.concept.oo;
+
+public class TestaFuncionario {
+	public static void main(String[] args) {
+		Funcionario righetto = new Funcionario();
+		
+		righetto.setSalario(1300.30);
+		righetto.setDepartamento("TI");
+		righetto.setNomeCompleto("Danilo Righetto");
+		righetto.setRg("1234567-8");
+		righetto.recebeAumento(200);
+		
+		System.out.println("Salario com aumento: " + righetto.getSalario());
+		System.out.println("Ganho anual: " + righetto.calculaGanhoAnual());
+	}
+}
