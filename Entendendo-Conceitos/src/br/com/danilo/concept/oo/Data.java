379a8707b651a7a6cf9b454077e018ccb533b1fd
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-19 15:15:40 -0200

    Construtores Exercicio 3

--- Entendendo-Conceitos/src/br/com/danilo/concept/oo/Data.java
+++ Entendendo-Conceitos/src/br/com/danilo/concept/oo/Data.java
@@ -4,12 +4,30 @@ public class Data {
 	private int dia;
 	private int mes;
 	private int ano;
+	int max[] = new int[12];
 	
 	public Data(int dia, int mes, int ano) {
-		super();
-		this.dia = dia;
-		this.mes = mes;
-		this.ano = ano;
+		max[1] = 31;
+		max[2] = 28;
+		max[3] = 31;
+		max[4] = 30;
+		max[5] = 31;
+		max[6] = 30;
+		max[7] = 31;
+		max[8] = 31;
+		max[9] = 30;
+		max[10] = 31;
+		max[11] = 30;
+		max[12] = 31;
+		
+		if(mes <= 12 && max[mes] <= dia) {
+			this.dia = dia;
+			this.mes = mes;
+			this.ano = ano;
+		}else {
+			System.out.println("Não foi possível definir a data!");
+			System.out.println("Por favor insira uma data valida!");
+		}
 	}
 	
 	public int getDia() {
@@ -34,4 +52,26 @@ public class Data {
 	public String getDataFormatada() {
 		return this.getDia()+"/"+this.getMes()+"/"+this.getAno();
 	}
+	
+	/* Solução do Desafio 3 */
+	private boolean isDataViavel(int dia, int mes, int ano) {
+        if (dia <= 0 || mes <= 0) {
+            return false;
+        }
+
+        int ultimoDiaDoMes = 31; // por padrao são 31 dias
+        if (mes == 4 || mes == 6 || mes == 9 || mes == 11 ) {
+            ultimoDiaDoMes = 30;
+        } else if (mes == 2) {
+            ultimoDiaDoMes = 28;
+            if((ano % 400 == 0) || ((ano % 4 == 0) && (ano % 100 != 0))){
+                ultimoDiaDoMes = 29;
+            }
+        }
+        if (dia > ultimoDiaDoMes) {
+            return false;
+        }
+
+        return true;
+    }
 }
