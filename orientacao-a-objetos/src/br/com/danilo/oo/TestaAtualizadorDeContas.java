a376ac7636139dad24e8adc14e60948aecb0dd19
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-23 21:13:55 -0200

    Herança, reescrita e polimorfismo - Exercicio 7

---
+++ orientacao-a-objetos/src/br/com/danilo/oo/TestaAtualizadorDeContas.java
@@ -0,0 +1,29 @@
+package br.com.danilo.oo;
+
+/**
+ * @author danilo righetto
+ * @version 0.1
+ * @category herança, reescrita e polimorfismo
+ *
+ */
+public class TestaAtualizadorDeContas {
+	/* Herança, reescrita e polimorfismo - Exercicio 7 - inicio */
+	public static void main(String[] args) {
+        Conta c = new Conta();
+        Conta cc = new ContaCorrente();
+        Conta cp = new ContaPoupanca();
+
+        c.deposita(1000); 
+        cc.deposita(1000); 
+        cp.deposita(1000);
+
+        AtualizadorDeContas adc = new AtualizadorDeContas(0.01);
+
+        adc.roda(c);
+        adc.roda(cc);
+        adc.roda(cp);
+
+        System.out.println("Saldo Total: " + adc.getSaldoTotal());
+    }
+	/* Herança, reescrita e polimorfismo - Exercicio 7 - fim */
+}
