d386f3f680ed7066b1d4d0fe9026f0d1856a3fbd
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-23 20:58:28 -0200

    Herança, reescrita e polimorfismo - Exercicio 4

---
+++ orientacao-a-objetos/src/br/com/danilo/oo/TestaContas.java
@@ -0,0 +1,29 @@
+package br.com.danilo.oo;
+
+/**
+ * @author danilo righetto
+ * @version 0.1
+ * @category herança, reescrita e polimorfismo
+ *
+ */
+public class TestaContas {
+	public static void main(String[] args) {
+		/* Herança, reescrita e polimorfismo - Exercicio 4 - inicio */
+		Conta c = new Conta();
+        ContaCorrente cc = new ContaCorrente();
+        ContaPoupanca cp = new ContaPoupanca();
+
+        c.deposita(1000); 
+        cc.deposita(1000); 
+        cp.deposita(1000);
+
+        c.atualiza(0.01);
+        cc.atualiza(0.01);
+        cp.atualiza(0.01);
+
+        System.out.println(c.getSaldo());
+        System.out.println(cc.getSaldo());
+        System.out.println(cp.getSaldo());
+        /* Herança, reescrita e polimorfismo - Exercicio 4 - fim */
+	}
+}
