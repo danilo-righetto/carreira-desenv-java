832e00ed3abf53dc569462c87a1aac196f233358
Danilo Righetto <danilonewtrue@gmail.com>
2018-01-23 21:01:11 -0200

    Herança, reescrita e polimorfismo - Exercicio 5

--- orientacao-a-objetos/src/br/com/danilo/oo/TestaContas.java
+++ orientacao-a-objetos/src/br/com/danilo/oo/TestaContas.java
@@ -9,10 +9,17 @@ package br.com.danilo.oo;
 public class TestaContas {
 	public static void main(String[] args) {
 		/* Herança, reescrita e polimorfismo - Exercicio 4 - inicio */
+		
+		/* Herança, reescrita e polimorfismo - Exercicio 5
 		Conta c = new Conta();
         ContaCorrente cc = new ContaCorrente();
         ContaPoupanca cp = new ContaPoupanca();
-
+		*/
+		
+		Conta c = new Conta();
+		Conta cc = new ContaCorrente();
+		Conta cp = new ContaPoupanca();
+		
         c.deposita(1000); 
         cc.deposita(1000); 
         cp.deposita(1000);
